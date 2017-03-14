cddae52d2f872978ed52ea8b25984a20427cfe97
kojonek2 <kojonek2@wp.pl>
2017-03-14 12:29:41 +0100

    decimal numbers

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,80 +20,111 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        long currentNumber;
+        long currentNumber = 0;
+        bool isDecimal = false;
+        long divider = 1;
  
         // x is appended to currentNumber
         void AppendNumber(long x)
         {
-            if(currentNumber<1000000000000000 && currentNumber > -1000000000000000)
+            if(currentNumber<100000000000000 && currentNumber > -100000000000000)
             {
                 currentNumber = currentNumber * 10 + x;
+                if(isDecimal)
+                {
+                    divider *= 10;
+                }
+            }
+        }
+
+        // Display currentNumber in TextBlock
+        void ShowNumber()
+        {
+            Double number = (Double)currentNumber / (Double)divider;
+            if(isDecimal && divider == 1)
+            {
+                TextBox_Result.Text = number.ToString() + ",";
+            }
+            else
+            {
+                TextBox_Result.Text = number.ToString();
             }
         }
 
         public MainWindow()
         {
             InitializeComponent();
+            ShowNumber();
         }
 
         private void Button_1_Click(object sender, RoutedEventArgs e)
         {
             AppendNumber(1);
-            TextBox_Result.Text = currentNumber.ToString();
+            ShowNumber();
         }
 
         private void Button_2_Click(object sender, RoutedEventArgs e)
         {
             AppendNumber(2);
-            TextBox_Result.Text = currentNumber.ToString();
+            ShowNumber();
         }
 
         private void Button_3_Click(object sender, RoutedEventArgs e)
         {
             AppendNumber(3);
-            TextBox_Result.Text = currentNumber.ToString();
+            ShowNumber();
         }
 
         private void Button_4_Click(object sender, RoutedEventArgs e)
         {
             AppendNumber(4);
-            TextBox_Result.Text = currentNumber.ToString();
+            ShowNumber();
         }
 
         private void Button_5_Click(object sender, RoutedEventArgs e)
         {
             AppendNumber(5);
-            TextBox_Result.Text = currentNumber.ToString();
+            ShowNumber();
         }
 
         private void Button_6_Click(object sender, RoutedEventArgs e)
         {
             AppendNumber(6);
-            TextBox_Result.Text = currentNumber.ToString();
+            ShowNumber();
         }
 
         private void Button_7_Click(object sender, RoutedEventArgs e)
         {
             AppendNumber(7);
-            TextBox_Result.Text = currentNumber.ToString();
+            ShowNumber();
         }
 
         private void Button_8_Click(object sender, RoutedEventArgs e)
         {
             AppendNumber(8);
-            TextBox_Result.Text = currentNumber.ToString();
+            ShowNumber();
         }
 
         private void Button_9_Click(object sender, RoutedEventArgs e)
         {
             AppendNumber(9);
-            TextBox_Result.Text = currentNumber.ToString();
+            ShowNumber();
         }
 
         private void Button_0_Click(object sender, RoutedEventArgs e)
         {
             AppendNumber(0);
-            TextBox_Result.Text = currentNumber.ToString();
+            ShowNumber();
+        }
+
+        private void Button_Comma_Click(object sender, RoutedEventArgs e)
+        {
+            if(isDecimal == false)
+            {
+                isDecimal = true;
+                divider = 1;
+                ShowNumber();
+            }
         }
     }
 }
